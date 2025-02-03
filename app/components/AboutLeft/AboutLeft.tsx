291fe56b1307da253f233c9675df784be2490434
Ehtesham-Zahid <shamimalick321@gmail.com>
2025-02-03 18:32:20 +0500

    Making Responsive

--- app/components/AboutLeft/AboutLeft.tsx
+++ app/components/AboutLeft/AboutLeft.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export const AboutLeft = () => {
   return (
-    <div className="px-4 py-10 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-lg xl:max-w-screen-xl md:px-24 lg:px-8 lg:py-12">
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-lg xl:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
       <div className="w-full mb-16">
         <h1 className="text-4xl text-center syne-font text-[#072661] font-bold mb-5">
           Coverage Engine
